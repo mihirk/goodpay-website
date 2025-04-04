561adaed653159980255178ddca69b6debbedccb
mihirk <mihir.khatwani@gmail.com>
2025-04-04 17:29:27 +0100

    Adding google tag event"

--- src/app/campaign/1/cta.tsx
+++ src/app/campaign/1/cta.tsx
@@ -1,11 +1,15 @@
 'use client';
 import {mailtoLink} from "@/app/campaign/1/email";
-import {sendGAEvent} from "@next/third-parties/google";
+import {sendGAEvent, sendGTMEvent} from "@next/third-parties/google";
 
+const sendGoogleTrackingEvent = () => {
+    sendGAEvent('event', 'buttonClicked', {value: 'campaign-1'});
+    sendGTMEvent({event: 'buttonClicked', value: 'campaign-1'});
+}
 export default function CTA() {
     return (<a
         href={mailtoLink}
-        onClick={() => sendGAEvent('event', 'buttonClicked', {value: 'campaign-1'})}
+        onClick={sendGoogleTrackingEvent}
         className="flex flex-col items-center justify-center px-8 py-4 bg-red-500 hover:bg-red-600 rounded-xl shadow-md hover:shadow-lg transition duration-300"
     >
               <span className="text-lg font-semibold text-white">
