6fc1ae289884fb08cbcc2128f2c5fd8a9e987e17
mihirk <mihir.khatwani@gmail.com>
2025-04-04 16:31:19 +0100

    Mobile and web design

--- src/app/campaign/1/page.tsx
+++ src/app/campaign/1/page.tsx
@@ -3,7 +3,7 @@ import {testingMailToLink} from './email';
 
 export default function Campaign() {
     return (
-        <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-50">
+        <main className="flex flex-col items-center justify-start md:justify-center min-h-screen p-4 pt-6 bg-gray-50">
             <h1 className="text-3xl font-bold text-center mb-6">
                 {"Did You Know You're Paying a 3% "}
                 <span className="text-red-600">TAX</span> to Visa & Mastercard Every Time You Tap To Pay?
